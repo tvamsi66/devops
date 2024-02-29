3fe98b54580b3c99971af75f781646d6ebee363c
tvamsi66 <tvamsi66@yahoo.com>
2024-02-29 08:17:45 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@
 #akamai help
 
 # Add any other commands you want to run here
-#akamai list
-#akamai --version
+akamai list
+akamai --version
 
 echo "Received user input from github workflow: $USER_INPUT"
 
