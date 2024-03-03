534d15ea1ebff82c02c51e69149f40a5e119c5a5
tvamsi66 <tvamsi66@yahoo.com>
2024-03-02 21:35:25 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,22 @@
 # Echo Akamai CLI version
 akamai --version
 
+# Update to stable rule set
+#akamai pipeline crf -p "$PIPELINE_NAME" -r v2023-01-05
+
+#Run a merge
+akamai pipeline merge -p "$PIPELINE_NAME" $ENVIRONMENT
+
+#Update the property in ION
+#akamai pm set-ruleformat v2023-01-05
+akamai pm property-update -p "$ENVIRONMENT"$PROPERTY_NAME --file $WORKSPACE/$PIPELINE_NAME/dist/"$ENVIRONMENT".$PROPERTY_NAME.papi.json
+
+#Activate the property in ION
+akamai property-manager activate-version -p $ENVIRONMENT.$PROPERTY_NAME --network $AKAMAI_NETWORK
+
+#--------------------------------------------------
+ls -R /app/
+
 akamai property-manager lg
 
 akamai pipeline lc
@@ -12,6 +28,3 @@ echo "Received user input from github workflow: $AKAMAI_NETWORK"
 echo "Received user input from github workflow: $ENVIRONMENT"
 echo "Received user input from github workflow: $PIPELINE_NAME"
 echo "Received user input from github workflow: $PROPERTY_NAME"
-
-
-ls -R /app/
